a6f868c3829c40b326085daa1163639527e5d17a
julen <julen.uribarren@alumni.mondragon.edu>
2018-12-21 09:31:38 +0100

    Added user data view. SpringController.java and tiles.xml edited. userData.jsp added.

--- hero_x_force/src/main/java/edu/mondragon/controller/SpringController.java
+++ hero_x_force/src/main/java/edu/mondragon/controller/SpringController.java
@@ -232,4 +232,23 @@ public class SpringController {
 		
 		return view;
 	}
+	
+	/**
+	 * @brief Method that shows the view to edit the user data
+	 * @param model implementation of Map for use when building data model
+	 * @return String
+	 */
+	@RequestMapping(value = "/userData", method = RequestMethod.GET)
+	public String showUserData(HttpServletRequest request, HttpServletResponse response, Model model) {
+		String view = "home";
+		
+		HttpSession session = request.getSession(true);
+		if(session.getAttribute("user") != null) {
+			view = "userData";
+		} else {
+			model.addAttribute("error", "general.notLogged");
+		}
+		
+		return view;
+	}
 }
